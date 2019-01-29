4354bf67d775a3c7b9fc2d3263f0c18a715b2062
StevenvL <stevenluu4x4@gmail.com>
2019-01-28 23:02:42 -0800

    Finish Encoding

--- hw1/src/hw1/Encoding.java
+++ hw1/src/hw1/Encoding.java
@@ -3,13 +3,48 @@ package hw1;
 import java.util.*;
 
 public class Encoding {
+	static Set<String> storage;
+	public Encoding() {
+		storage = new TreeSet<>();
+	}
 
 	public static Set<String> morseCodes(int m, int n) {
 		Set<String> result = new TreeSet<>();
-		return null;
+		String dot = ".";
+		String dash = "-";
+		String start = "";
+		
+		for(int i = 0; i < m; i++) {
+			start += dot;
+		}
+		for(int i = 0; i < n ; i++) {
+			start += dash;
+		}
+		
+		permutationHelper(start,"");
+		result = storage;
+		return result;
+	}
+	
+	public static void permutationHelper(String remainder, String soFar) {
+		if(remainder.isEmpty()) {
+			storage.add(soFar);
+		}
+		else {
+			for(int i = 0; i<remainder.length(); i++) {
+				String letter = String.valueOf(remainder.charAt(i));
+				String prefix = remainder.substring(0, i  );
+				String suffix = remainder.substring(i+1);
+				permutationHelper(prefix+suffix, soFar + letter);
+			}
+		}
 	}
 	
 	public static void main (String args[]) {
-	
+		Encoding tester = new Encoding();
+		tester.morseCodes(2, 1);
+		for(String s : storage) {
+			System.out.println(s);
+		}
 	}
 }
--- hw1/src/hw1/Strings.java
+++ hw1/src/hw1/Strings.java
@@ -11,10 +11,11 @@ public class Strings {
 		for(int i = 0; i < str.length(); i++) {
 			for(int j = i+1; j < str.length(); j++) {
 				if(str.charAt(i) == str.charAt(j))
-					dupes.add(str.charAt(i));
+					dupes.add(str.charAt(i));	// If there is a dupe add it to the treeset.
 			}
 		}
-		for(int i = 0; i<str.length(); i++) {
+		
+		for(int i = 0; i<str.length(); i++) {	// If the treeset does not contain the current char of str, add to result.
 			if(!dupes.contains(str.charAt(i)))
 				result += str.charAt(i);
 		}
