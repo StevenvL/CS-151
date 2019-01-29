94e6d520cc4e4e470bb685183524e2afd2d4423f
StevenvL <stevenluu4x4@gmail.com>
2019-01-28 18:16:27 -0800

    Set up

---
+++ hw1/src/hw1/Encoding.java
@@ -0,0 +1,15 @@
+package hw1;
+
+import java.util.*;
+
+public class Encoding {
+
+	public static Set<String> morseCodes(int m, int n) {
+		Set<String> result = new TreeSet<>();
+		return null;
+	}
+	
+	public static void main (String args[]) {
+	
+	}
+}
---
+++ hw1/src/hw1/Strings.java
@@ -0,0 +1,11 @@
+package hw1;
+
+public class Strings {
+	public static String uniqueLetters(String str) {
+		return null;
+	}
+
+	public static void main(String args[]) {
+
+	}
+}
