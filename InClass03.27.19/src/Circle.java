a26f3b7632cae6638bd54d4c24004d635780e2bc
StevenvL <stevenluu4x4@gmail.com>
2019-03-27 11:30:47 -0700

    Start In Class Assignment. Refactor Code

---
+++ InClass03.27.19/src/Circle.java
@@ -0,0 +1,22 @@
+public class Circle  {
+    private final double radius;
+    final double pi = Math.PI;
+    
+    public Circle() {
+        this(1);
+    }
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    
+
+    public double area() {
+        // A = π r^2
+        return pi * Math.pow(radius, 2);
+    }
+    
+    public double perimeter() {
+        // P = 2πr
+        return 2 * pi * radius;
+    }
+}
